b52c32f81856c62a303225b6821499e1cc7bafbb
Jamie Long <jtlong3rd@gmail.com>
2016-07-11 15:50:23 -0400

    Setup server stubs; move static html to public/ directory

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var partials = require('express-partials');
+
+var app = express();
+
+app.configure(function() {
+  app.use(partials());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+});
+
+module.exports = app;
