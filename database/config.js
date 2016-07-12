a9f085d83e9c6d3dc9901ab4506cb32557d17739
Jamie Long <jtlong3rd@gmail.com>
2016-07-11 22:50:34 -0400

    Update Heroku URI for mongo

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/collab-comp-db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/collab-comp-db');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
