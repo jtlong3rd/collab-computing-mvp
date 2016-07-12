85fc4d3a5c15fa15086252ca41c5d4ea7387375f
Jamie Long <jtlong3rd@gmail.com>
2016-07-11 22:09:58 -0400

    Add config file for database

---
+++ compiled/database/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/collab-comp-db');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongodb connection open');
+});
+
+module.exports = db;
